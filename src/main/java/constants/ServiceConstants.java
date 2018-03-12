d0781c6e6c96980f888fa4ce610d391c989bbc89
John Santhosh <jsanthos@asu.edu>
2018-03-12 12:08:27 -0700

    Changes to start the spring-boot slave app on EC2 startup.

--- src/main/java/constants/ServiceConstants.java
+++ src/main/java/constants/ServiceConstants.java
@@ -4,8 +4,12 @@ public interface ServiceConstants {
     String PATH_SEPARATOR = "/";
     int SLEEP_TIME_1S = 1000;
     int SLEEP_TIME_20S = 20000;
-    String EC2_AMI_ID = "ami-cc2aa0b4";
+    String EC2_AMI_ID = "ami-065ccd7e";
     String AWS_ACCESS_KEY_ID = "AKIAJ23PYR3ZPWNMBJJA";
     String AWS_SECRET_ACCESS_KEY = "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";
     int MAX_EC2_INSTANCE_COUNT = 19;
+    String SPRING_BOOT_STARTUP_SLAVE = "#!/usr/bin/env bash"
+            + "\n" + "sudo su ubuntu"
+            + "\n" + "cd /home/ubuntu/image-service-slave"
+            + "\n" + "mvn spring-boot:run";
 }
--- src/main/java/service/impl/Ec2ServiceImpl.java
+++ src/main/java/service/impl/Ec2ServiceImpl.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.InstanceType;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
+import constants.ServiceConstants;
 import model.Ec2Instance;
 import service.Ec2Service;
 
@@ -29,6 +30,20 @@ public class Ec2ServiceImpl implements Ec2Service {
         runInstancesRequest.setInstanceType("t2.micro");
         runInstancesRequest.setKeyName("cloud-computing");
         runInstancesRequest.setSecurityGroups(Arrays.asList("cloud-computing"));
+
+        //TODO Need to solve the AWS credentials issue on startup.
+        /*
+        ssh into the newly create instance. "cat /var/log/cloud-init-output.log" to get the cloud init logs.
+
+        com.amazonaws.SdkClientException: Unable to load AWS credentials from any provider in the chain
+        at com.amazonaws.auth.AWSCredentialsProviderChain.getCredentials(AWSCredentialsProviderChain.java:131)
+
+        Application gets started but, it fails to listen to the SQS.
+         */
+
+        // Runs a bash script to startup the spring-boot application on EC2 startup.
+        runInstancesRequest.setUserData(org.apache.commons.codec.binary.Base64
+                .encodeBase64String(ServiceConstants.SPRING_BOOT_STARTUP_SLAVE.getBytes()));
         RunInstancesResult runResponse = ec2Instance.getEc2Client().runInstances(runInstancesRequest);
         String instanceId = runResponse.getReservation().getInstances().get(0).getInstanceId();
         ec2Instance.setAwsInstanceId(instanceId);
