6f77b89a7f330b0c288a8c65b902518c09192952
John Santhosh <jsanthos@asu.edu>
2018-03-27 00:43:26 -0700

    Updated the slave startup script. Updated S3 results bucket name.

--- src/main/java/constants/ServiceConstants.java
+++ src/main/java/constants/ServiceConstants.java
@@ -6,9 +6,6 @@ public interface ServiceConstants {
     String AWS_ACCESS_KEY = "amazon.ec2.access.key.id";
     String AWS_SECRET_KEY = "amazon.ec2.secret.access.key";
     String SPRING_BOOT_STARTUP_SLAVE = "#!/usr/bin/env bash"
-            +"\n"+ "pip install awscli  --upgrade --user"
-            +"\n" + "/home/ubuntu/.local/bin/aws configure set aws_access_key_id AKIAJ23PYR3ZPWNMBJJA"
-            +"\n" + "/home/ubuntu/.local/bin/aws configure set aws_secret_access_key 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"
             + "\n" + "sudo su ubuntu"
             + "\n" + "cd /home/ubuntu/image-service-slave"
             + "\n" + "java -jar target/image-service-slave-1.0-SNAPSHOT.jar";
